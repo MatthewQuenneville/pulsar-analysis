ceca202ee45cc02e864db99933b434f441f69f44
Matthew Quenneville <mattq1@gmail.com>
2015-06-22 16:44:34 -0400

    Allow for main pulse - inter pulse pairs in correlations

--- pulseCorr.py
+++ pulseCorr.py
@@ -21,6 +21,9 @@ searchRes=1.0/10000
 # Only take the brightest pulse in each file
 onePulsePerFile=True
 
+def gaussian(x,mu,sig,A):
+    return A*np.exp(-0.5*(x-mu)*(x-mu)/sig/sig)
+
 # Find correlation coefficient between two spectra
 def corr(spec1,spec2):
     spec1_norm=(spec1-np.mean(spec1))/np.std(spec1)
@@ -32,18 +35,19 @@ def corr(spec1,spec2):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "Usage: %s waterfallDir/" % sys.argv[0]
-        # Run the code as eg: ./pulseCorr.py waterfallDir/ .
+        print "Usage: %s MPDir/ IPDir/" % sys.argv[0]
+        # Run the code as eg: ./pulseCorr.py MPDir/ IPDir/.
         sys.exit(1)
     
     # Declare observation list, and dynamic spectra, frequency band, pulse time, and RFI-free channel dictionaries
-    pulseDict={}
-    freqBand={}
-    print "Finding Pulses..."
+    MPDict={}
+    IPDict={}
+
+    print "Finding MP Pulses..."
     # Loop through JB and GMRT files
     for ifilename in os.listdir(sys.argv[1]):
-        
-        # Folded spectrum axes: time, frequency, phase, pol=4 (XX, XY, YX, YY).
+        if not('foldspec' in ifilename or 'waterfall' in ifilename):
+            continue
 
         # Get run information
         deltat=pf.getDeltaT(ifilename)
@@ -107,43 +111,137 @@ if __name__ == "__main__":
             pulseRange=range(i[0]-leadBins,i[0]+trailBins)
             offRange=range(i[0]-leadBins-(leadBins+trailBins),i[0]-leadBins)
 
-            pulseDict[pulseTime]={}
+            MPDict[pulseTime]={}
 
-            pulseDict[pulseTime]['dynSpec']=ps.dynSpec(w,indices=pulseRange,
+            MPDict[pulseTime]['dynSpec']=ps.dynSpec(w,indices=pulseRange,
                                                        normChan=False)
-            pulseDict[pulseTime]['spec']=pulseDict[pulseTime]['dynSpec'].sum(1)
-            pulseDict[pulseTime]['prof']=pulseDict[pulseTime]['dynSpec'].sum(0)
+            MPDict[pulseTime]['spec']=MPDict[pulseTime]['dynSpec'].sum(1)
+            MPDict[pulseTime]['prof']=MPDict[pulseTime]['dynSpec'].sum(0)
 
-            pulseDict[pulseTime]['dynSpec_bg']=ps.dynSpec(w,indices=offRange,
+            MPDict[pulseTime]['dynSpec_bg']=ps.dynSpec(w,indices=offRange,
                                                        normChan=False)
-            pulseDict[pulseTime]['spec_bg']=pulseDict[pulseTime][
-                'dynSpec_bg'].sum(1)
-            pulseDict[pulseTime]['prof_bg']=pulseDict[pulseTime][
-                'dynSpec_bg'].sum(0)
-       
+            MPDict[pulseTime]['spec_bg']=MPDict[pulseTime]['dynSpec_bg'].sum(1)
+            MPDict[pulseTime]['prof_bg']=MPDict[pulseTime]['dynSpec_bg'].sum(0)
+    print "Complete!"
+    print "Finding IP Pulses..."
+    for ifilename in os.listdir(sys.argv[2]):
+        if not('foldspec' in ifilename or 'waterfall' in ifilename):
+            continue
+
+        # Get run information
+        deltat=pf.getDeltaT(ifilename)
+        telescope=pf.getTelescope(ifilename)
+        startTime=pf.getStartTime(ifilename)
+
+        if 'foldspec' in ifilename:
+            f = np.load(sys.argv[2]+ifilename)
+            ic = np.load(sys.argv[2]+ifilename.replace('foldspec', 'icount'))
+
+            # Collapse time axis
+            f=f[0,...]
+            ic=ic[0,...]
+
+            # Find populated bins
+            fullList=np.flatnonzero(ic.sum(0).sum(0))
+            w=f/ic[...,np.newaxis]
+
+            binWidth=deltat/f.shape[1]
+
+        elif 'waterfall' in ifilename:
+            w=np.load(sys.argv[2]+ifilename)
+            w=np.swapaxes(w,0,1)
+            fullList=range(w.shape[1])
+            binWidth=pf.getWaterfallBinWidth(telescope,w.shape[0])
+
+        else:
+            print "Error, unrecognized file type."
+            sys.exit()
+
+        # Check for polarization data
+        if not w.shape[-1]==4:
+            print "Error, polarization data is missing for "+startTime+"."
+            sys.exit()
+
+        # Rebin to find giant pulses
+        nSearchBins=min(w.shape[1],int(round(deltat/searchRes)))
+    
+        w_rebin=pf.rebin(w,nSearchBins)
+        timeSeries_rebin=pf.getTimeSeries(w_rebin)
+        timeSeries=pf.getTimeSeries(w)
+        pulseList=pf.getPulses(timeSeries_rebin,binWidth=searchRes,threshold=20)
+        if nSearchBins<w.shape[1]:
+            pulseList=[(pf.resolvePulse(
+                        timeSeries,int(pos*w.shape[1]/nSearchBins),
+                        binWidth=binWidth,searchRadius=1.0/10000),height) 
+                       for (pos,height) in pulseList]
+        if len(pulseList)==0:
+            print "Warning, no pulse found for start time:"
+            print startTime.iso
+        elif onePulsePerFile:
+            pulseList=[pulseList[0]]
+
+        # Find range of pulse to examine
+        leadBins=int(leadWidth/binWidth)
+        trailBins=int(trailWidth/binWidth)
+
+        # Assign pulse values in dictionary
+        for i in pulseList:
+            pulseTime=pf.getTime(i[0],binWidth,startTime)
+            pulseRange=range(i[0]-leadBins,i[0]+trailBins)
+            offRange=range(i[0]-leadBins-(leadBins+trailBins),i[0]-leadBins)
+
+            IPDict[pulseTime]={}
+
+            IPDict[pulseTime]['dynSpec']=ps.dynSpec(w,indices=pulseRange,
+                                                       normChan=False)
+            IPDict[pulseTime]['spec']=IPDict[pulseTime]['dynSpec'].sum(1)
+            IPDict[pulseTime]['prof']=IPDict[pulseTime]['dynSpec'].sum(0)
+
+            IPDict[pulseTime]['dynSpec_bg']=ps.dynSpec(w,indices=offRange,
+                                                       normChan=False)
+            IPDict[pulseTime]['spec_bg']=IPDict[pulseTime]['dynSpec_bg'].sum(1)
+            IPDict[pulseTime]['prof_bg']=IPDict[pulseTime]['dynSpec_bg'].sum(0)
+
     print "Complete!\n"
     print "Calculating correlations..."
     
     # Find all pairs of pulses
-    pulsePairs=itertools.combinations(pulseDict.keys(),2)
-
+    MPMPpairs=itertools.combinations(MPDict.keys(),2)
+    MPIPpairs=itertools.product(MPDict.keys(),IPDict.keys())
     # Create lists of time difference and correlation coefficient for
     # each pair of pulses
-    dt=[]
-    corrCoef=[]
-    for (i,j) in pulsePairs:
-        dt.append(abs((i-j).sec))
-        spec1=(pulseDict[i]['spec'][:,(0,3)]-pulseDict[i][
+    MPMPdt=[]
+    MPMPcorrCoef=[]
+    for (i,j) in MPMPpairs:
+        MPMPdt.append(abs((i-j).sec))
+        spec1=(MPDict[i]['spec'][:,(0,3)]-MPDict[i][
                 'spec_bg'][:,(0,3)]).sum(-1)
-        spec2=(pulseDict[j]['spec'][:,(0,3)]-pulseDict[j][
+        spec2=(MPDict[j]['spec'][:,(0,3)]-MPDict[j][
                 'spec_bg'][:,(0,3)]).sum(-1)
-        corrCoef.append(corr(spec1,spec2))
+        MPMPcorrCoef.append(corr(spec1,spec2))
+
+    MPIPdt=[]
+    MPIPcorrCoef=[]
+    for (i,j) in MPIPpairs:
+        MPIPdt.append(abs((i-j).sec))
+        spec1=(MPDict[i]['spec'][:,(0,3)]-MPDict[i][
+                'spec_bg'][:,(0,3)]).sum(-1)
+        spec2=(IPDict[j]['spec'][:,(0,3)]-IPDict[j][
+                'spec_bg'][:,(0,3)]).sum(-1)
+        MPIPcorrCoef.append(corr(spec1,spec2))
+
     print "Complete!"
     
     # Plot results
+    gauss_x=np.logspace(-2,2,1000)
+    gauss_y=gaussian(gauss_x,0.,25.,1./3)    
     plt.figure()
-    plt.scatter(dt,corrCoef)
+    plt.scatter(MPMPdt,MPMPcorrCoef,c='r',s=40,label='MP-MP')
+    plt.scatter(MPIPdt,MPIPcorrCoef,c='b',s=40,label='MP-IP')
+    plt.plot(gauss_x,gauss_y,'k',label='Model')
+    plt.legend(loc=3)
     plt.xlim(0.01,100)
+    plt.ylim(-0.4,0.7)
     plt.xscale('log')
     plt.ylabel('Correlation Coefficient')
     plt.xlabel('Time lag (s)')
