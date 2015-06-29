1429e9e1af6ed00c0edef866756f4d61ca0d8fb0
Matthew Quenneville <mattq1@gmail.com>
2015-06-29 13:34:02 -0400

    Correct Fourier Transforms, and remove lmfit dependence

--- pulseProjFreq.py
+++ pulseProjFreq.py
@@ -7,15 +7,8 @@ import pulseFinder as pf
 from math import factorial
 import warnings
 import pulseSpec as ps
-doFits=True
-try:
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        from lmfit.models import ExponentialGaussianModel
-        from lmfit import Parameters
-except ImportError:
-    print "lmfit module not found. Exponentially modified gaussian fits will not be performed."
-    doFits=False
+from scipy.special import erfc
+from scipy.optimize import curve_fit
 
 # Resolution to use for searching in seconds. Must be larger than or
 # equal to phase bin size.
@@ -25,6 +18,11 @@ pulseWidth=0.0001 # 100 microseconds
 
 leadWidth=0.0001
 trailWidth=0.0003
+
+def expModGauss(x,sigma):#,mu,sigma,gamma):
+    mu=0.0
+    gamma=1.0
+    return (gamma/2.)*np.exp((gamma/2)*(2*mu+gamma*sigma*sigma-2*x))*erfc((mu+gamma*sigma*sigma-x)/(np.sqrt(2)*sigma))
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -108,6 +106,10 @@ if __name__ == "__main__":
     freqList=[freqBand[0]+i*(freqBand[1]-freqBand[0])/spec.shape[0] 
               for i in range(spec.shape[0])]
 
+    
+    spec=spec[1:]
+    freqList=freqList[1:]
+
     if spec.shape[-1]==4:
         f,((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2,sharex='col',sharey='row')
         ax1.plot(freqList,spec[:,0],'.')
@@ -147,58 +149,34 @@ if __name__ == "__main__":
         xmax=max(max(spec2),max(spec1))
         
         bins=np.linspace(np.floor(xmin),np.ceil(xmax),50)
-        binCenters=np.array([(bins[i+1]+bins[i])/2. for i in range(len(bins)-1)])
+        x_fine=np.linspace(np.floor(xmin),np.ceil(xmax),1000)
+
         f,(ax1,ax2) = plt.subplots(1,2,sharex='col',sharey='row')
-        specHist1=ax1.hist(spec1,normed=True,bins=bins)
-        print spec1.std(), spec1.mean()
+        specHist1=ax1.hist(spec1,normed=True,bins=bins,label='Data')
+        binCenters=np.array([(bins[i+1]+bins[i])/2. for i in range(len(bins)-1)
+                             if not(specHist1[0][i]==0)])
+        binEntries=np.array([i for i in specHist1[0] if not i==0])
+        popt,pcov=curve_fit(expModGauss,binCenters,binEntries,
+                            sigma=np.power(binEntries,0.5))
+        if np.std(spec1)>1.0:
+            ax1.plot(x_fine,expModGauss(x_fine,np.sqrt(np.var(spec1)-1.0)),label='Estimated')
+        ax1.plot(x_fine,expModGauss(x_fine,popt[0]),label='Fitted')
         #ax1.set_yscale('log')
-        if doFits:
-            expGaussMod=ExponentialGaussianModel()
-            pars=Parameters()
-            pars.add('amplitude',value=1.0,vary=False)
-            pars.add('gamma',value=1.0,vary=False)
-            pars.add('center',value=0.0,vary=False)
-            if spec1.std()>1.:
-                pars.add('sigma',value=np.sqrt(spec1.std()**2-1.0),vary=True)
-            else:
-                pars.add('sigma',value=0.0,vary=True)
-            out=expGaussMod.fit(specHist1[0],pars,x=binCenters)
-            print "===================="
-            print "Polarization 0"
-            print "Constrained Parameters:"
-            print "Amplitude: ", out.params.valuesdict()['amplitude']
-            print "Lambda: ", out.params.valuesdict()['gamma']
-            print "Center: ", out.params.valuesdict()['center'], '\n'
-            print "Free Parameters:"
-            print "Sigma: ", out.params.valuesdict()['sigma']
-            print "====================\n"
-            ax1.plot(binCenters, out.best_fit)
+        ax1.legend()
         ax1.set_xlim(min(bins),max(bins))
         ax1.set_xlabel("Intensity")
         ax1.set_title('Polarization 0')
-        specHist2=ax2.hist(spec2,normed=True,bins=bins)
-        if doFits:
-            expGaussMod=ExponentialGaussianModel()
-            pars=Parameters()
-            pars.add('amplitude',value=1.0,vary=False)
-            pars.add('gamma',value=1.0,vary=False)
-            pars.add('center',value=0.0,vary=False)
-            if spec1.std()>1.:
-                pars.add('sigma',value=np.sqrt(spec1.std()**2-1.0),vary=True)
-            else:
-                pars.add('sigma',value=0.0,vary=True)
-            out=expGaussMod.fit(specHist2[0],pars,x=binCenters)
-            print "===================="
-            print "Polarization 3"
-            print "Constrained Parameters:"
-            print "Amplitude: ", out.params.valuesdict()['amplitude']
-            print "Lambda: ", out.params.valuesdict()['gamma']
-            print "Center: ", out.params.valuesdict()['center'], '\n'
-            print "Free Parameters:"
-            print "Sigma: ", out.params.valuesdict()['sigma']
-            print "====================\n"
-            ax2.plot(binCenters, out.best_fit)
+        specHist2=ax2.hist(spec2,normed=True,bins=bins,label='Data')
+        binCenters=np.array([(bins[i+1]+bins[i])/2. for i in range(len(bins)-1)
+                             if not(specHist2[0][i]==0)])
+        binEntries=np.array([i for i in specHist2[0] if not i==0])
+        popt,pcov=curve_fit(expModGauss,binCenters,binEntries,
+                            sigma=np.power(binEntries,0.5))
+        if np.std(spec2)>1.0:
+            ax2.plot(x_fine,expModGauss(x_fine,np.sqrt(np.var(spec2)-1.0)),label='Estimated')
+        ax2.plot(x_fine,expModGauss(x_fine,popt[0]),label='Fitted')
         #ax2.set_yscale('log')
+        ax2.legend()
         ax2.set_title('Polarization 3')
         ax2.set_xlim(min(bins),max(bins))
         ax2.set_xlabel("Intensity")
@@ -212,29 +190,16 @@ if __name__ == "__main__":
         xmax=max(specNorm)
         
         bins=np.linspace(np.floor(xmin),np.ceil(xmax),50)
-        binCenters=np.array([(bins[i+1]+bins[i])/2. for i in range(len(bins)-1)])
-        specHist=plt.hist(specNorm,normed=True,bins=bins)
-        if doFits:
-            expGaussMod=ExponentialGaussianModel()
-            pars=Parameters()
-            pars.add('amplitude',value=1.0,vary=False)
-            pars.add('gamma',value=1.0,vary=False)
-            pars.add('center',value=0.0,vary=False)
-            if spec1.std()>1.:
-                pars.add('sigma',value=np.sqrt(spec1.std()**2-1.0),vary=True)
-            else:
-                pars.add('sigma',value=0.0,vary=True)
-            out=expGaussMod.fit(specHist[0],pars,x=binCenters)
-            print "===================="
-            print "Polarization 3"
-            print "Constrained Parameters:"
-            print "Amplitude: ", out.params.valuesdict()['amplitude']
-            print "Lambda: ", out.params.valuesdict()['gamma']
-            print "Center: ", out.params.valuesdict()['center'], '\n'
-            print "Free Parameters:"
-            print "Sigma: ", out.params.valuesdict()['sigma']
-            print "====================\n"
-            ax2.plot(binCenters, out.best_fit)
+        x_fine=np.linspace(np.floor(xmin),np.ceil(xmax),1000)
+        specHist=plt.hist(specNorm,normed=True,bins=bins,label='Data')
+        binCenters=np.array([(bins[i+1]+bins[i])/2. for i in range(len(bins)-1) if not(specHist[0][i]==0)])
+        binEntries=np.array([i for i in specHist[0] if not i==0])
+        popt,pcov=curve_fit(expModGauss,binCenters,binEntries,
+                            sigma=np.power(binEntries,0.5))
+        if np.std(spec)>1.0:
+            ax1.plot(x_fine,expModGauss(x_fine,np.sqrt(np.var(spec)-1.0)),label='Estimated')
+        plt.plot(x_fine,expModGauss(x_fine,popt[0]),label='Fitted')
+        plt.legend()
         plt.yscale('log')
         plt.xlim(min(bins),max(bins))
         plt.xlabel("Intensity")
@@ -242,17 +207,18 @@ if __name__ == "__main__":
 
     if spec.shape[-1]==4:
         f,((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2,sharex='col',sharey='row')
-        sample=np.linspace(0,spec.shape[0]/(freqBand[1]-freqBand[0])/2,spec.shape[0]/2)
-        ax1.plot(sample,np.abs(np.fft.fft(spec[:spec.shape[0]/2,0])),'.')
+        sample=np.fft.fftshift(np.fft.fftfreq(
+                spec.shape[0],(freqBand[1]-freqBand[0])/spec.shape[0]))
+        ax1.plot(sample,np.abs(np.fft.fftshift(np.fft.fft(spec[:,0]))),'.')
         ax1.set_ylabel('abs(Fourier Amplitude)')
         ax1.set_title('Polarization 0')
-        ax2.plot(sample,np.abs(np.fft.fft(spec[:spec.shape[0]/2,3])),'.')
+        ax2.plot(sample,np.abs(np.fft.fftshift(np.fft.fft(spec[:,3]))),'.')
         ax2.set_title('Polarization 3')
-        ax3.plot(sample,np.abs(np.fft.fft(spec[:spec.shape[0]/2,1])),'.')
+        ax3.plot(sample,np.abs(np.fft.fftshift(np.fft.fft(spec[:,1]))),'.')
         ax3.set_ylabel('abs(Fourier Amplitude)')
         ax3.set_xlabel('Delay (microseconds)')
         ax3.set_title('Polarization 1')
-        ax4.plot(sample,np.abs(np.fft.fft(spec[:spec.shape[0]/2,2])),'.')
+        ax4.plot(sample,np.abs(np.fft.fftshift(np.fft.fft(spec[:,2]))),'.')
         ax4.set_xlabel('Delay (microseconds)')
         ax4.set_title('Polarization 2')
         plt.suptitle('Fourier Transforms of Spectra')
@@ -260,15 +226,18 @@ if __name__ == "__main__":
         
         complexSpec=spec[:,1]+1j*spec[:,2]
         plt.figure()
-        plt.plot(sample,np.abs(np.fft.fft(complexSpec[:spec.shape[0]/2])),'.')
+        plt.plot(sample,np.abs(np.fft.fftshift(np.fft.fft(complexSpec))),'.')
         plt.xlabel('Delay (microseconds)')
         plt.ylabel('abs(Fourier Amplitude)')
-        plt.title('Fourier transform of complex cross-correlation')
+        plt.title('Fourier transform of complex cross-spectra')
         plt.show()
+        delay=sample[np.argmax(
+                np.abs(np.fft.fftshift(np.fft.fft(complexSpec))))]
+        print "Offset (R-L): "+str(int(round(delay*1000)))+ " ns."
     else:
         plt.figure()
         sample=np.linspace(0,spec.shape[0]/(freqBand[1]-freqBand[0])/2,spec.shape[0]/2)
-        plt.plot(sample,np.abs(np.fft.fft(spec[:spec.shape[0]/2])),'.')
+        plt.plot(sample,np.abs(np.fft.fft(spec)[:spec.shape[0]/2]),'.')
         plt.ylabel('abs(Fourier Amplitude)')
         plt.title('Fourier Transform of Spectrum')
         plt.xlabel('Delay (microseconds)')
